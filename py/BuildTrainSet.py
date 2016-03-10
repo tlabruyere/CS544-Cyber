b4b335ba5d6117d7c6d839f8a37e8ea06d98f0e0
Tom LaBruyere <tom.labruyere@gmail.com>
2016-03-10 06:08:33 +0000

    add initial train set

--- py/BuildTrainSet.py
+++ py/BuildTrainSet.py
@@ -1,12 +1,19 @@
 #/usr/bin/python
 
 import numpy as np
+#import sklearn.feature_extraction.text as CountVectorizer 
+import sklearn
 
 class BuildTrainData:
+    byteExt = '.bytes'
+    asmExt = '.asm'
+
     def build(self, csvLabels, trainDir):
        data = np.loadtxt(open(csvLabels, 'rb'), delimiter=',')
+       return data
 
 if __name__=='__main__':
     loader = BuildTrainData()
-    dat = loader.build('/var/data/trainLabels.csv', '/var/data/train')
+    dat = loader.build('/home/tom/idLabels.csv', '/var/data/train')
+    print dat.shape
     print dat[1,:]
