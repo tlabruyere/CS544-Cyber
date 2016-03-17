8ece08b7b2b77917326e7064668d8bc32cc44416
Esteban Guillen <esteban@Estebans-MacBook-Pro.local>
2016-03-16 20:56:17 -0600

    starting to build ngram vocab for naive bayes classifier

---
+++ tmp/generateHexDumpNGrams.py
@@ -0,0 +1,37 @@
+
+
+import os
+
+trainDir = "/Volumes/Seagate Backup Plus Drive/data/train/"
+
+ngrams = {}
+count = 1
+
+def parseNGrams(s):
+    tokens = s.split(' ')
+    for i in range(len(tokens)-3):
+        ngram = tokens[i] + tokens[i+1] + tokens[i+2] + tokens[i+3]
+        if ngram in ngrams:
+            ngrams[ngram] = ngrams[ngram] + 1
+        else:
+            ngrams[ngram] = 1
+
+
+for fileName in os.listdir(trainDir):
+    s = ''
+    if fileName.endswith(".bytes"):
+        if( count == 1 or count == 2 or count == 3 or count == 4 or count == 5 or count == 6 or count == 7 or count == 8 or count == 100 or count == 500 or count == 1000 or count == 2000 or count == 4000 or count == 5000):
+            print(count)
+        count = count + 1
+        if count > 20:
+            break
+        with open(trainDir + fileName) as f:
+            for line in f:
+                s = line[9:].rstrip('\n')
+                parseNGrams(s)
+
+
+print("printing dict")
+for w in sorted(ngrams, key=ngrams.get, reverse=True):
+    if ngrams[w] > 10:
+        print(w, ngrams[w])
