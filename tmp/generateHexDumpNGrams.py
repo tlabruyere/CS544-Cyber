5b1892731dcef3ef6c877537b68daf2fad52bc21
Esteban Guillen <esteban.j.guillen@gmail>
2016-03-19 23:06:33 +0000

    creates 2grams from bytes file and writes them to file

--- tmp/generateHexDumpNGrams.py
+++ tmp/generateHexDumpNGrams.py
@@ -8,8 +8,11 @@ ngrams = {}
 
 def parseNGrams(s):
     tokens = s.split(' ')
-    for i in range(len(tokens)-3):
-        ngram = tokens[i] + tokens[i+1] + tokens[i+2] + tokens[i+3]
+    for i in range(len(tokens)-2):
+        ngram_s = tokens[i] + tokens[i+1]
+        ngram = 0
+        if '?' not in ngram_s:
+            ngram = int(ngram_s,16)
         if ngram in ngrams:
             ngrams[ngram] = ngrams[ngram] + 1
         else:
@@ -20,17 +23,29 @@ for fileName in os.listdir(trainDir):
     
     if fileName.endswith(".bytes"):
         count = count + 1
-        if count % 100 == 0:
+        if count % 1000 == 0:
             print(count)
+        #if count > 5000:
+            #break
         with open(trainDir + fileName) as f:
             for line in f:
                 parseNGrams(line[9:].rstrip())
 
 print("writing to file")
+c = 1
 #todo save most common ngrams to file: ngram, ngramId, count
-for w in sorted(ngrams, key=ngrams.get, reverse=True):
-    if ngrams[w] > 100:
-        #write ngrams that apper more than 100 times
-        with open("topNGrams.csv","w") as f:
-            f.write(w + " " + ngrams[w] + "\n") 
+with open("topNGrams.csv","w") as f:
+    for w in sorted(ngrams, key=ngrams.get, reverse=True):
+    #for w in ngrams: 
+        if ngrams[w] > 1:
+            #if c % 10000 == 0:
+                #print(c)
+            #c = c + 1
+            #write ngrams that apper more than 100 times
+            f.write(str(w) + " " + str(ngrams[w]) + "\n") 
             #print(w, ngrams[w])
+            c = c + 1
+            if c % 100 == 0:
+                print(c)
+            #if c > 500:
+                #break
